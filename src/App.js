4da6f591c5ca86e3d0e2999f85015b92a701fc35
Danaye7 <danielayele077@gmail.com>
2023-12-19 17:16:18 +0300

    connecting with my fake backend
    q
    Q
    X
    
    |
    /
    cls
    clear
    exit
    exit
    
    |
    
    exit
    Q
    q
    q

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 // import './App.css';
 import Header from './components/Header.js';
 import Tasks from './components/Tasks.js'
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import AddTask from './components/AddTask'
 
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id:1,
-      text: 'Doctors Appointment',
-      day: 'Feb 4th at 1:45pm',
-      reminder: true
-    },
-    {
-      id:2,
-      text: 'Meeting at School',
-      day: 'Feb 5th at 3:45pm',
-      reminder: true
-    },
-    {
-      id:3,
-      text: 'Food Shopping',
-      day: 'Feb 6th at 2:30pm',
-      reminder: false
-    },
-  ]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    const fetchTasks = async ()=> {
+      const res = await fetch('http://localhost:5000/tasks');
+      
+      if(res.ok){
+        const data = await res.json();
+        setTasks(data);
+      }
+
+    }
+    fetchTasks();
+  }, []);
 
     // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch (`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
     setTasks((tasks).filter((task)=> task.id !== id));
 }
 
@@ -42,21 +40,37 @@ function App() {
 
     // Add Task
 
-  const addTask = (newTask) => {
-    // Find the maximum ID from the existing tasks or set to 0 if no tasks exist
-    const newId = Math.max(...tasks.map((task) => task.id), 0) + 1;
-  
-    // Create a new array of tasks including the new task with an incremented ID
-    const updatedTasks = [...tasks, { ...newTask, id: newId }];
-  
-    // Set the state of tasks to the updatedTasks array
-    setTasks(updatedTasks);
+  const addTask = async (newTask) => {
+
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTask)
+    });
+    
+    if(res.ok){
+      const data = await res.json();
+      setTasks([...tasks, data]);
+    }
+
+    /*
+      // Find the maximum ID from the existing tasks or set to 0 if no tasks exist
+        const newId = Math.max(...tasks.map((task) => task.id), 0) + 1;
+    
+      // Create a new array of tasks including the new task with an incremented ID
+        const updatedTasks = [...tasks, { ...newTask, id: newId }];
+    
+      // Set the state of tasks to the updatedTasks array
+        setTasks(updatedTasks);
+    */
   };
   
   return (
     <div className="container">
-      <Header />
-      <AddTask  onAdd={addTask} />
+      <Header onAdd = {()=> {setShowAddTask(!showAddTask)}} showAdd = {showAddTask} />
+      {showAddTask && <AddTask  onAdd={addTask} />}
       {tasks.length > 0 ?
       (
       <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
